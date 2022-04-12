2afd2b1c92e47a06c829f54d7b1ac509b393654a
Benjamin F <84413548+bforrest-pct@users.noreply.github.com>
2022-04-11 21:45:27 -0700

    adding mock DOM

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -4,7 +4,15 @@ export default class App extends React.Component {
   render() {
     return (
       <div>
-        Todo App
+        <h1>Todo</h1>
+        <ul>
+        <li>Walk the Dog</li>
+        <li>Chase the Postman</li>
+        <li>State your intentions</li>
+        <li>Retreat back home</li>
+        </ul>
+        <form><input/><button>Submit</button></form>
+        <button>Hide Completed</button>
       </div>
     )
   }
