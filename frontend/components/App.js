04d2a8c695c8f3c00a017dc0988e6797758aa8c9
Benjamin F <84413548+bforrest-pct@users.noreply.github.com>
2022-04-11 22:03:37 -0700

    transitioned to state based dom

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,17 +1,50 @@
 import React from 'react'
 
 export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: [
+        {
+          name: 'Walk the Dog',
+          id: 1528817077286,
+          completed: false
+        },
+        {
+          name: 'Chase the Postman',
+          id: 1528817077287,
+          completed: false
+        },
+        {
+          name: 'State your intentions',
+          id: 1528817077288,
+          completed: false
+        },
+        {
+          name: 'Retreat back home',
+          id: 1528817077289,
+          completed: false
+        }
+      ]
+    }
+  }
   render() {
+    const {todos} = this.state;
     return (
       <div>
         <h1>Todo</h1>
         <ul>
-        <li>Walk the Dog</li>
-        <li>Chase the Postman</li>
-        <li>State your intentions</li>
-        <li>Retreat back home</li>
+        {
+          todos.map(todo=>{
+            return (<li key={todo.id}>{todo.name}</li>)
+          })
+        }
         </ul>
-        <form><input/><button>Submit</button></form>
+
+        <form>
+          <input/>
+          <button>Submit</button>
+          </form>
         <button>Hide Completed</button>
       </div>
     )
